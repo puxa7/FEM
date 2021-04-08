638b391445391749afb709c902d48ebb7410165c
puxa7 <umpik123@gmail.com>
2021-04-08 12:58:14 +0200

    pierwszy commit

---
+++ poczatkujacy.py
@@ -0,0 +1,9 @@
+paliwo = 8.5 * (67*0.1 + 124*0.15 + 11.7*0.2)
+autostrada = 124*0.17
+prom = 50
+koszt = paliwo + autostrada + prom
+print (koszt)
+print (koszt / 2)
+print (koszt / 3)
+print (koszt / 4)
+print (koszt / 5)

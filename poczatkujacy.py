41fa76a8dfffd993b23eb13c169345184f957a90
puxa7 <umpik123@gmail.com>
2021-04-08 13:18:49 +0200

    drugi commit

--- poczatkujacy.py
+++ poczatkujacy.py
@@ -1,3 +1,4 @@
+'''
 paliwo = 8.5 * (67*0.1 + 124*0.15 + 11.7*0.2)
 autostrada = 124*0.17
 prom = 50
@@ -7,3 +8,19 @@ print (koszt / 2)
 print (koszt / 3)
 print (koszt / 4)
 print (koszt / 5)
+'''
+'''
+jakas_zmienna = "To jest jakis napis" 
+print (jakas_zmienna) 
+jakas_zmienna = 8.5 
+print (jakas_zmienna)
+'''
+
+'''lista = [1,2,3,4,"napis", True, False, 8.5, 11]
+print(lista)
+'''
+
+'''
+slownik = {"jablecznik" : "ciasto z jablek", "czy lubisz jablecznik" : True}
+print(slownik)
+'''
